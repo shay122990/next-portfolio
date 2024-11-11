880ba938118fbbab3a687fb128463be8e59b3e17
Shay <moeshakasanova@gmail.com>
2024-11-11 14:37:43 +0400

    github and linked in links added to nav

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
 
 const Navbar = () => {
@@ -13,6 +13,16 @@ const Navbar = () => {
     { id: 3, link: "portfolio" },
     { id: 4, link: "experience" },
     { id: 5, link: "contact" },
+    {
+      id: 6,
+      link: "https://github.com/shay122990",
+      icon: <FaGithub size={24} />,
+    },
+    {
+      id: 7,
+      link: "https://www.linkedin.com/in/shay-asanova90/",
+      icon: <FaLinkedin size={24} />,
+    },
   ];
 
   return (
@@ -30,12 +40,18 @@ const Navbar = () => {
       </div>
 
       <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
+        {links.map(({ id, link, icon }) => (
           <li
             key={id}
             className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
           >
-            <Link href={`#${link}`}>{link}</Link>
+            {icon ? (
+              <a href={link} target="_blank" rel="noreferrer">
+                {icon}
+              </a>
+            ) : (
+              <Link href={`#${link}`}>{link}</Link>
+            )}
           </li>
         ))}
       </ul>
@@ -49,14 +65,25 @@ const Navbar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, icon }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
-              <Link onClick={() => setNav(!nav)} href={`#${link}`}>
-                {link}
-              </Link>
+              {icon ? (
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={() => setNav(!nav)}
+                >
+                  {icon}
+                </a>
+              ) : (
+                <Link onClick={() => setNav(!nav)} href={`#${link}`}>
+                  {link}
+                </Link>
+              )}
             </li>
           ))}
         </ul>
@@ -66,3 +93,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
