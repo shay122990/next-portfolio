5e7bf08d54db383a8382c0938145e96e96fa5378
Shay <moeshakasanova@gmail.com>
2024-11-12 13:37:49 +0400

    removed linkedin link, change colors:

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { useState } from "react";
-import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub } from "react-icons/fa";
 import Image from "next/image";
 
 const Navbar = () => {
@@ -17,11 +17,6 @@ const Navbar = () => {
       id: 6,
       link: "https://github.com/shay122990",
       icon: <FaGithub size={24} />,
-    },
-    {
-      id: 7,
-      link: "https://www.linkedin.com/in/shay-asanova90/",
-      icon: <FaLinkedin size={24} />,
     },
   ];
 
@@ -43,7 +38,7 @@ const Navbar = () => {
         {links.map(({ id, link, icon }) => (
           <li
             key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 hover:text-lime-400 duration-200 link-underline"
           >
             {icon ? (
               <a href={link} target="_blank" rel="noreferrer">
@@ -58,13 +53,13 @@ const Navbar = () => {
 
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        className="cursor-pointer pr-4 z-10 text-white md:hidden hover:text-lime-500"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white">
           {links.map(({ id, link, icon }) => (
             <li
               key={id}
@@ -93,4 +88,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
