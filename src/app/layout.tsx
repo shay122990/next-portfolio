822cc2668218863a585629381a27f8c994f6bead
Shay <moeshakasanova@gmail.com>
2024-11-18 03:58:35 +0400

    head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import Head from "next/head";
 import "./globals.css";
 import { Providers } from "./providers";
 import type { Metadata } from "next";
 import { Coda } from "next/font/google";
 
-const coda = Coda({ subsets: ["latin"],  weight: ['400'], });
+const coda = Coda({ subsets: ["latin"], weight: ['400'], });
 
 export const metadata: Metadata = {
   title: "Shay Asanova",
@@ -20,6 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <meta name="description" content="ReactJS, NextJs developer in Dubai" />
+      </Head>
       <body className={coda.className}>
         <Providers>{children}</Providers>
       </body>
