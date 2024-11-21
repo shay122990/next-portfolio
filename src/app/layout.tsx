fabe98c47dd2959acc903df54d9c6da269d6db54
Shay <moeshakasanova@gmail.com>
2024-11-21 14:48:08 +0400

    correct path to favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
-import Head from "next/head";
 import "./globals.css";
 import { Providers } from "./providers";
 import type { Metadata } from "next";
 import { Coda } from "next/font/google";
 
-const coda = Coda({ subsets: ["latin"], weight: ['400'], });
+const coda = Coda({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Shay Asanova",
   description: "ReactJS, NextJs developer in Dubai",
   icons: {
-    icon: "/favicon.png",
+    icon: "/images/favicon.ico", 
   },
 };
 
@@ -21,10 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <meta name="description" content="ReactJS, NextJs developer in Dubai" />
-      </Head>
       <body className={coda.className}>
         <Providers>{children}</Providers>
       </body>
