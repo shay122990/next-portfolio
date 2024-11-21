844b9f8511e2725b44c2fd50a27e906b9781cdcc
Shay <moeshakasanova@gmail.com>
2024-11-21 16:23:49 +0400

    set width and height for experienceCategories

--- src/app/components/experience-categories/ExperienceCategories.tsx
+++ src/app/components/experience-categories/ExperienceCategories.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from "react";
 import Image from "next/image";
-import servicesData from "@/app/page-data/tech-stack-data"; 
+import servicesData from "@/app/page-data/tech-stack-data";
 
 type ServiceCategory = keyof typeof servicesData;
 
@@ -13,27 +13,31 @@ export default function ExperienceCategories() {
   };
 
   return (
-    <div className="w-full h-full max-w-4xl mx-auto">
-     <div className="flex place-content-center mb-8 md:mb-12 lg:mb-16 mt-8 md:mt-12 lg:mt-16">
-      {Object.keys(servicesData).map((tab) => (
-        <button
-          key={tab}
-          className={`py-2 px-6 text-lg ${
-            activeTab === tab
-              ? "border-2 border-green-500"
-              : "text-gray-600 hover:text-green-500"
-          } md:px-8 lg:px-10`}
-          onClick={() => handleTabClick(tab as ServiceCategory)}
-        >
-          {tab}
-        </button>
-      ))}
-    </div>
+    <div className="w-full h-[600px] lg:h-[700px] max-w-5xl mx-auto mt-10">
+      <div className="flex place-content-center mb-8 md:mb-12 lg:mb-16 mt-8 md:mt-12 lg:mt-16">
+        {Object.keys(servicesData).map((tab) => (
+          <button
+            key={tab}
+            className={`py-2 px-6 text-lg ${
+              activeTab === tab
+                ? "border-2 border-green-500"
+                : "text-gray-600 hover:text-green-500"
+            } md:px-8 lg:px-10`}
+            onClick={() => handleTabClick(tab as ServiceCategory)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
       <div>
         {servicesData[activeTab].map(({ id, paragraph, image }) => (
           <div key={id} className="flex mb-8 space-x-6">
             <div className="flex-shrink-0">
-              <Image src={image} alt={`Tech Image ${id}`} className="h-24 w-24 object-cover rounded-lg" />
+              <Image
+                src={image}
+                alt={`Tech Image ${id}`}
+                className="h-24 w-24 object-cover rounded-lg"
+              />
             </div>
             <div>
               <p className="text-gray-700">{paragraph}</p>
@@ -42,9 +46,5 @@ export default function ExperienceCategories() {
         ))}
       </div>
     </div>
-  )
+  );
 }
-
-
-
- 
