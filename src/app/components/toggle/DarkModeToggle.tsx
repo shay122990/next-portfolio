2095a2d8eb1cb7796fb9a6ac2de40466879f2cf4
Shay <moeshakasanova@gmail.com>
2024-11-22 00:51:09 +0400

    useId for darkmode input toggle

--- src/app/components/toggle/DarkModeToggle.tsx
+++ src/app/components/toggle/DarkModeToggle.tsx
@@ -1,40 +1,44 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { useTheme } from "next-themes"; 
+import React, { useState, useEffect, useId } from "react";
+import { useTheme } from "next-themes";
 
 export const DarkModeToggle: React.FC = () => {
   const { theme, setTheme } = useTheme();
   const [isDark, setIsDark] = useState(false);
+  const uniqueId = useId(); 
 
   useEffect(() => {
     if (theme !== undefined) {
       setIsDark(theme === "dark");
     }
   }, [theme]);
+
   const toggleTheme = () => {
     const newTheme = isDark ? "light" : "dark";
     setTheme(newTheme);
-    setIsDark(!isDark); 
+    setIsDark(!isDark);
   };
 
   return (
     <div className="flex items-center bg-transparent">
       <input
         type="checkbox"
-        id="dark-mode-toggle"
+        id={`dark-mode-toggle-${uniqueId}`} 
         onChange={toggleTheme}
         checked={isDark}
         className="hidden"
       />
       <label
-        htmlFor="dark-mode-toggle"
+        htmlFor={`dark-mode-toggle-${uniqueId}`} 
         className={`relative w-16 h-8 block rounded-full cursor-pointer transition-all duration-300 shadow-inner ${
           isDark ? "bg-white" : "bg-gray-300"
         }`}
       >
         <span
           className={`absolute w-6 h-6 rounded-full top-1 left-1 transition-all duration-300 transform ${
-            isDark ? "translate-x-8 bg-gradient-to-r from-blue-700 to-red-400" : "bg-gradient-to-r from-orange-400 to-yellow-300"
+            isDark
+              ? "translate-x-8 bg-gradient-to-r from-blue-700 to-red-400"
+              : "bg-gradient-to-r from-orange-400 to-yellow-300"
           }`}
         ></span>
         <i className={`absolute text-white text-lg left-1 top-1 ${isDark ? "hidden" : "inline-block"}`}>
