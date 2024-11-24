b330babb5908ebd123c335fcc584b6d3ae19a5af
Shay <moeshakasanova@gmail.com>
2024-11-25 00:30:54 +0400

    moved projects data to page data

---
+++ src/app/page-data/project-data.js
@@ -0,0 +1,45 @@
+const projects = [
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project1",
+    altText: "Project 1",
+    projectName: "Project One",
+  },
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project2",
+    altText: "Project 2",
+    projectName: "Project Two",
+  },
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project3",
+    altText: "Project 3",
+    projectName: "Project Three",
+  },
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project4",
+    altText: "Project 4",
+    projectName: "Project Four",
+  },
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project5",
+    altText: "Project 5",
+    projectName: "Project Five",
+  },
+  {
+    imageSrc: "/images/hero-image.jpg",
+    liveLink: "https://example.com",
+    githubLink: "https://github.com/username/project6",
+    altText: "Project 6",
+    projectName: "Project Six",
+  },
+];
+export default projects;
