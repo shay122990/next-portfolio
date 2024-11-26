a5cafe66ab6a53da8ae72e288c489163bc4e57ea
Shay <moeshakasanova@gmail.com>
2024-11-27 00:40:58 +0400

    contact from layout and text

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -5,10 +5,10 @@ export default function Contact() {
   return (
     <section className="w-full">
       <Divider text="Get in touch" id="contact" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-center min-h-[600px]">
-        <div className="flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-center min-h-[600px] px-10">
+        <div className="flex flex-col justify-center items-center xsm: text-center sm:text-center md:text-left">
           <h2 className="text-xl font-semibold mb-4 w-96">Let&apos;s Grab a Coffee! ☕️</h2>
-          <p className="text-gray-700 items-center w-96">
+          <p className="text-gray-500 dark:text-green-400 items-center w-96">
             I&apos;m excited to collaborate with you on
             your project and contributing to your organization&apos;s success. Let&apos;s connect
             and discuss how I can bring that vision to life. <br /> Please fill
