e3b4900cdce142cbe0e32d1545c2ea29a373f1df
Shay <moeshakasanova@gmail.com>
2024-11-30 00:16:09 +0400

    about section layout

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section className="w-full flex flex-col items-center">
       <Divider text="About Me" id="about" />
-      <article className="w-full px-1 flex flex-row text-center items-center justify-center gap-96">
+      <article className="w-full px-1 flex flex-col lg:flex-row text-center items-center justify-center gap-6 lg:gap-40">
         <Image
           src="/images/logo.gif"
           alt="Profile Picture"
@@ -13,20 +13,20 @@ const About = () => {
           height={200}
           className="h-60 w-60 object-cover rounded-sm"
         />
-        <div className='w-2/5'>
-        <h2>So, who am I?</h2>
-        <p>
-          <br />
-          I&apos;ve always been drawn to the magic of technology and
-          design coming together. That&apos;s what led me to become a developer.
-          <br />
-          With a strong handle on frontend technologies, I&apos;m here to bring your
-          vision to life, blending beautiful design with seamless functionality,
-          speed, compatibility, and accessibility.
-          <br />I love keeping up with the latest trends in development and
-          design so I can always deliver the best for your company and personal
-          projects.
-        </p>
+        <div className="w-full lg:w-2/5 px-10">
+          <h2>So, who am I?</h2>
+          <p>
+            <br />
+            I&apos;ve always been drawn to the magic of technology and
+            design coming together. That&apos;s what led me to become a developer.
+            <br />
+            With a strong handle on frontend technologies, I&apos;m here to bring your
+            vision to life, blending beautiful design with seamless functionality,
+            speed, compatibility, and accessibility.
+            <br />I love keeping up with the latest trends in development and
+            design so I can always deliver the best for your company and personal
+            projects.
+          </p>
         </div>
       </article>
     </section>
