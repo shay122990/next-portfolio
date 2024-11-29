79a66171581a58d32bbacd64f4bb61937dbeacc4
Shay <moeshakasanova@gmail.com>
2024-11-29 12:03:00 +0400

    about section layout

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,10 +1,19 @@
 import Divider from '../divider/Divider';
+import Image from 'next/image';
 
 const About = () => {
   return (
     <section className="w-full flex flex-col items-center">
       <Divider text="About Me" id="about" />
-      <article className="w-96 flex flex-col text-center">
+      <article className="w-full px-1 flex flex-row text-center items-center justify-center gap-96">
+        <Image
+          src="/images/logo.gif"
+          alt="Profile Picture"
+          width={200}
+          height={200}
+          className="h-60 w-60 object-cover rounded-sm"
+        />
+        <div className='w-2/5'>
         <h2>So, who am I?</h2>
         <p>
           <br />
@@ -18,6 +27,7 @@ const About = () => {
           design so I can always deliver the best for your company and personal
           projects.
         </p>
+        </div>
       </article>
     </section>
   );
