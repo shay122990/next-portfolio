03ad1f7db3b1cf08ba588c079083813235d61cb6
Shay <moeshakasanova@gmail.com>
2024-11-29 11:44:18 +0400

    list accessibility fix

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,12 +1,10 @@
 "use client";
+
 import Link from "next/link";
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 import { TiThMenu } from "react-icons/ti";
-
-
-
 import Image from "next/image";
 import { DarkModeToggle } from "../toggle/DarkModeToggle";
 
@@ -14,7 +12,7 @@ type NavLink = {
   id: number;
   link: string;
   icon?: JSX.Element;
-  label?: string; 
+  label?: string;
 };
 
 const Navbar = () => {
@@ -27,7 +25,7 @@ const Navbar = () => {
       id: 3,
       link: "https://github.com/shay122990",
       icon: <FaGithub size={24} />,
-      label: "GitHub Profile", 
+      label: "GitHub Profile",
     },
   ];
 
@@ -38,10 +36,10 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center w-full mt-2 pb-2 px-4 dark:bg-grey dark:text-white border-b border-green-500 relative">
-      {/* Mobile Menu Icons */}
+      {/* Mobile Menu Icon */}
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer z-10 text-white md:hidden "
+        className="cursor-pointer z-10 text-white md:hidden"
       >
         {nav ? (
           <FaTimes size={30} className="dark:text-white" />
@@ -52,6 +50,7 @@ const Navbar = () => {
 
       {/* Desktop Navigation */}
       <div className="hidden md:flex w-full justify-between items-center">
+        {/* Left Links */}
         <ul className="flex space-x-4">
           {leftLinks.map(({ id, link, icon, label }) => (
             <li
@@ -74,6 +73,7 @@ const Navbar = () => {
           ))}
         </ul>
 
+        {/* Logo */}
         <Link href="/">
           <Image
             src="/images/logo.gif"
@@ -85,17 +85,21 @@ const Navbar = () => {
           />
         </Link>
 
-        <ul className="flex space-x-4 items-center">
-          {rightLinks.map(({ id, link }) => (
-            <li
-              key={id}
-              className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-800 dark:text-white hover:scale-105 hover:text-green-500 duration-200 link-underline"
-            >
-              <Link href={`#${link}`}>{link}</Link>
-            </li>
-          ))}
+        {/* Right Links */}
+        <div className="flex items-center space-x-4">
+          <ul className="flex space-x-4">
+            {rightLinks.map(({ id, link }) => (
+              <li
+                key={id}
+                className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-800 dark:text-white hover:scale-105 hover:text-green-500 duration-200 link-underline"
+              >
+                <Link href={`#${link}`}>{link}</Link>
+              </li>
+            ))}
+          </ul>
+          {/* Dark Mode Toggle  */}
           <DarkModeToggle />
-        </ul>
+        </div>
       </div>
 
       {/* Mobile Logo */}
@@ -116,7 +120,7 @@ const Navbar = () => {
 
       {/* Mobile Navigation */}
       <ul
-        className={`absolute -top-2 left-0 w-full h-dvh  flex flex-col justify-center items-center text-white bg-gradient-to-r from-blue-500 to-teal-400 transform ${
+        className={`absolute -top-2 left-0 w-full h-dvh flex flex-col justify-center items-center text-white bg-gradient-to-r from-blue-500 to-teal-400 transform ${
           nav ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out`}
       >
@@ -131,7 +135,7 @@ const Navbar = () => {
                 href={link}
                 target="_blank"
                 rel="noreferrer"
-                aria-label={label} 
+                aria-label={label}
               >
                 {icon}
               </a>
