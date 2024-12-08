7da47424186008e6c635ef0eba50b96b33d30a13
Shay <moeshakasanova@gmail.com>
2024-12-08 11:15:20 +0400

    lcp fix

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="relative flex flex-row lg:flex-row justify-between items-center w-full h-80 lg:h-[35rem] px-4 lg:px-20  mt-1 lg:mt-8">
-      <div className="absolute inset-0 z-0 gradient-bg"></div>
+    <div className="gradient-bg flex flex-row lg:flex-row justify-between items-center w-full h-80 lg:h-[35rem] px-4 lg:px-20  mt-1 lg:mt-8">
 
       <motion.h1
-        className="text-blue-700 text-3xl md:text-4xl lg:text-8xl font-semibold lg:w-1/2 lg:text-left relative z-10"
+        className="text-blue-700 text-3xl md:text-4xl lg:text-8xl font-semibold lg:w-1/2 lg:text-left relative z-10 leading-0"
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
-        <span>Hi, I&apos;m Shay...</span>
-        <span className="block mt-4">A React developer in Dubai</span> 
+        Hi, I&apos;m Shay... <br /> <span className="block mt-5">A React developer in Dubai</span>
       </motion.h1>
 
       <motion.div
