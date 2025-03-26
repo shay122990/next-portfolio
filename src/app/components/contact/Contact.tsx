16816420485f6081d802e670ec335628968c756b
shay asanova <moeshakasanova@gmail.com>
2025-03-26 15:33:52 +0400

    style: form text alignment

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -5,12 +5,12 @@ export default function Contact() {
   return (
     <section className="w-full lg:px-10 px-4 mb-12">
       <Divider text="Get in touch" id="contact" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-center min-h-[600px] bg-black bg-opacity-20 rounded py-10 px-4">
-        <div className="flex flex-col justify-center items-center xsm: text-center sm:text-center md:text-left">
-          <h2 className="text-xl font-semibold mb-4 w-96">Let&apos;s Grab a Coffee! ☕️</h2>
-          <p className="dark:text-green-400 items-center w-96">
-           I’m excited to bring creative ideas and solutions to life! Whether you’re a company looking for someone passionate and skilled, or an individual with a vision for your next project, I’d love to connect. 
-           <br /> Fill out the form, and I’ll get back to you promptly. Let’s make something incredible happen together! 
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-center items-center min-h-[600px] bg-black bg-opacity-20 rounded gap-4 py-5 px-5">
+        <div className="lg:text-2xl flex flex-col lg:pl-20 justify-center items-center md:items-start md:text-left text-center">
+          <h2 className="font-semibold mb-4 max-w-md">Let&apos;s Grab a Coffee! ☕️</h2>
+          <p className="dark:text-green-400 max-w-md">
+            I’m excited to bring creative ideas and solutions to life! Whether you’re a company looking for someone passionate and skilled, or an individual with a vision for your next project, I’d love to connect. 
+            <br /> Fill out the form, and I’ll get back to you promptly.
           </p>
         </div>
         <div className="flex flex-1 justify-center items-center">
