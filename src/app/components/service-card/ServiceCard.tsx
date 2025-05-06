5c9ffc6c6c24d3e0a3bc56ac5576ec7e9e7e230e
shay asanova <moeshakasanova@gmail.com>
2025-05-07 00:11:06 +0400

    sevice card - style - bg change

--- src/app/components/service-card/ServiceCard.tsx
+++ src/app/components/service-card/ServiceCard.tsx
@@ -7,16 +7,19 @@ type ServiceCardProps = {
   image: string | StaticImageData;
 };
 
-const ServiceCard = ({ id,title, paragraph, image }: ServiceCardProps) => {
+const ServiceCard = ({ id, title, paragraph, image }: ServiceCardProps) => {
   return (
-    <div className="flex flex-col items-center text-center bg-white shadow-md rounded-lg p-4">
-      <Image
-        src={image}
-        alt={`Tech Image ${id}`}
-        className="h-40 w-40 object-cover rounded-md"
-      />
-      <span className="text-gray-700">{title}</span>
-      <p className="text-gray-700 mt-4">{paragraph}</p>
+    <div className="flex flex-col items-center text-center bg-gradient-to-b from-white via-gray-100 to-white  dark:from-blue-500 dark:to-teal-400 shadow-lg rounded-2xl p-6 transition-transform transform hover:scale-105 hover:shadow-2xl duration-300">
+      <div className="relative h-48 w-48 mb-4">
+        <Image
+          src={image}
+          alt={`Tech Image ${id}`}
+          fill
+          className="object-cover rounded-xl"
+        />
+      </div>
+      <h3 className="text-2xl font-bold text-gray-800 dark:text-white">{title}</h3>
+      <p className="text-gray-600 dark:text-gray-300 mt-3">{paragraph}</p>
     </div>
   );
 };
