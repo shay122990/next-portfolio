096210ac31002bfe1b0c64526d09e58795adfee3
shay asanova <moeshakasanova@gmail.com>
2025-05-07 00:26:11 +0400

    tab button - style - coclor change

--- src/app/components/tab-button/TabButton.tsx
+++ src/app/components/tab-button/TabButton.tsx
@@ -1,21 +1,23 @@
 type TabButtonProps = {
-    tab: string;
-    isActive: boolean;
-    onClick: () => void;
-  };
-  
-  const TabButton = ({ tab, isActive, onClick }: TabButtonProps) => {
-    return (
-      <button
-        className={`py-2 px-6 text-lg ${
-          isActive
-            ? "border-2 border-green-500"
-            : " hover:text-green-500"
-        }  lg:px-8 text-xs md:text-base lg:text-lg`}
-        onClick={onClick}
-      >
-        {tab}
-      </button>
-    );
-  };
-export default TabButton  
+  tab: string;
+  isActive: boolean;
+  onClick: () => void;
+};
+
+const TabButton = ({ tab, isActive, onClick }: TabButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`py-2 px-6 lg:px-8 text-md md:text-base  lg:text-lg font-semibold rounded text-gray-500 transition-all duration-300
+      ${
+        isActive
+          ? "bg-green-400 text-white border-2 border-green-400"
+          : "border-2  border-green-400 bg-white  hover:border-blue-500"
+      }`}
+    >
+      {tab}
+    </button>
+  );
+};
+
+export default TabButton;
