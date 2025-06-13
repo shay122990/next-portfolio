5613cde9d22a31d8ce5d90cd96f849379a25fd57
shay asanova <moeshakasanova@gmail.com>
2025-06-13 11:30:15 +0400

    logo-banner logo size change

--- src/app/components/logo-banner/LogoBanner.tsx
+++ src/app/components/logo-banner/LogoBanner.tsx
@@ -6,15 +6,18 @@ export default function LogoBanner() {
     <div className="relative w-full overflow-hidden">
       <div className="flex animate-scroll space-x-10">
         {[...Array(2)].map((_, index) => (
-          <div key={index} className="flex space-x-6">
+          <div key={index} className="flex space-x-12">
             {logos.map(({ id, imageUrl }) => (
-              <div key={id} className="flex items-center justify-center h-20 w-20">
+              <div
+                key={id}
+                className="flex items-center justify-center h-12 w-12"
+              >
                 <Image
                   src={imageUrl}
                   alt={`Logo ${id}`}
                   width={30}
                   height={30}
-                  className="object-contain logo-size"
+                  className="object-contain"
                 />
               </div>
             ))}
